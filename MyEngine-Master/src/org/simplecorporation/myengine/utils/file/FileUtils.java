f44cfe758f2541cea80e44ec5af009450f573565
2851999 <joelvdavies@btinternet.com>
2013-08-02 09:58:43 +0100

    * Development 3 *
    * Removed the previously added files then rewrote the FileUtils
    
    NOTE
    I have found gource :)

--- MyEngine-Master/src/org/simplecorporation/myengine/utils/file/FileUtils.java
+++ MyEngine-Master/src/org/simplecorporation/myengine/utils/file/FileUtils.java
@@ -1,48 +1,123 @@
 package org.simplecorporation.myengine.utils.file;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.LinkedList;
+
+import org.simplecorporation.myengine.utils.logger.Logger;
 
 public class FileUtils {
 	
-	/* Method to get a file path */
+	/* The method that returns the string of a file path */
 	public static String asFileString(String filePath) {
 		return new File(filePath).toPath().toString();
 	}
 	
-	/* Method to check if a file exists */
-	public static boolean doesFileExist(String filePath) {
+	/* The method that returns whether a file exists */
+	public static boolean doesExist(String filePath) {
 		return new File(asFileString(filePath)).exists();
 	}
 	
-	/* Method to check if a file is a file */
+	/* The method that returns whether a file is a file */
 	public static boolean isFile(String filePath) {
 		return new File(asFileString(filePath)).isFile();
 	}
 	
-	/* Method to check if a file is a directory */
+	/* The method that returns whether a file is a directory */
 	public static boolean isDirectory(String filePath) {
 		return new File(asFileString(filePath)).isDirectory();
 	}
 	
-	/* Method to read a file */
-	public static LinkedList<String> read(String filePath) {
-		//Create a text file object
-		TextFile textFile = new TextFile(filePath);
-		//Read the file
-		textFile.read();
-		//Return the file text
-		return textFile.fileText;
+	/* The method that returns whether a file is hidden */
+	public static boolean isHidden(String filePath) {
+		return new File(asFileString(filePath)).isHidden();
 	}
 	
-	/* Method to write a file */
-	public static void write(LinkedList<String> fileText , String filePath) {
-		//Create a text file object
-		TextFile textFile = new TextFile(filePath);
-		//Assign the file text
-		textFile.fileText = fileText;
-		//Write the file
-		textFile.write();
+	/* The method that returns whether a file can be read */
+	public static boolean canRead(String filePath) {
+		return new File(asFileString(filePath)).canRead();
+	}
+	
+	/* The method that returns whether a file can be written in */
+	public static boolean canWrite(String filePath) {
+		//If it returns false it may be because the file doesn't exist
+		//in the first place
+		return new File(asFileString(filePath)).canWrite();
+	}
+	
+	/* The method that reads a file */
+	public static LinkedList<String> read(String filePath) {
+		//The file text
+		LinkedList<String> fileText = new LinkedList<String>();
+		//Check whether the file exists
+		if (doesExist(filePath)) {
+			//Check whether the file is a file
+			if (isFile(filePath)) {
+				//Check whether the file can be read
+				if (canRead(filePath)) {
+					//Read the file and set the text in the linked list
+					try {
+						FileReader fileReader = new FileReader(asFileString(filePath));
+						BufferedReader bufferedReader = new BufferedReader(fileReader);
+						String line = "";
+						while ((line = bufferedReader.readLine()) != null) {
+							fileText.add(line);
+						}
+						bufferedReader.close();
+						fileReader.close();
+					} catch (FileNotFoundException e) {
+						e.printStackTrace();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				} else {
+					//Log a message
+					Logger.log("FileUtils read()" , "File cant be read " + filePath);
+				}
+			} else {
+				//Log a message
+				Logger.log("FileUtils read()" , "File not a file " + filePath);
+			}
+		} else {
+			//Log a message
+			Logger.log("FileUtils read()" , "File not found " + filePath);
+		}
+		//Return the file text
+		return fileText;
+	}
+	
+	public static void write(String filePath , LinkedList<String> fileText) {
+		//Check if the file path already exists
+		if (doesExist(filePath)) {
+			//Log a message
+			Logger.log("FileUtils write()" , "File already exists so will be overwritten " + filePath);
+		}
+		
+		try {
+			//Write the file
+			FileWriter fileWriter = new FileWriter(asFileString(filePath));
+			BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+			
+			//Write the first line
+			bufferedWriter.write(fileText.get(0));
+			for (int a = 1; a < fileText.size(); a++) {
+				bufferedWriter.newLine();
+				bufferedWriter.write(fileText.get(a));
+			}
+			
+			//Close the objects
+			bufferedWriter.close();
+			fileWriter.close();
+		} catch (IOException e) {
+			//Log a message
+			Logger.log("FileUtils write()" , "IOException writing the file " + filePath);
+			e.printStackTrace();
+		}
 	}
 	
 }
