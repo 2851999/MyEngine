37831d161d2b6d7532a30d7045b7f1a8bfa5df2f
2851999 <joelvdavies@btinternet.com>
2013-08-13 12:15:37 +0100

    * Development 9 (3) *
    * Added GUIPanel
    * GUIPanels now deal with components
    * GUI's now deal with GUIPanels

--- MyEngine-Master/src/org/simplecorporation/myengine/core/gui/GUI.java
+++ MyEngine-Master/src/org/simplecorporation/myengine/core/gui/GUI.java
@@ -17,65 +17,65 @@ import org.simplecorporation.myengine.utils.logger.Logger;
 public class GUI {
 	
 	/* The components in the GUI */
-	public LinkedList<GUIComponent> components;
+	public LinkedList<GUIPanel> panels;
 	
 	/* The constructor */
 	public GUI() {
 		//Create the linked list
-		this.components = new LinkedList<GUIComponent>();
+		this.panels = new LinkedList<GUIPanel>();
 	}
 	
 	/* The method to update the GUI */
 	public void updateGUI() {
 		//Update all of the components
-		for (int a = 0; a < this.components.size(); a++)
-			this.components.get(a).update();
+		for (int a = 0; a < this.panels.size(); a++)
+			this.panels.get(a).updatePanel();
 	}
 	
 	/* The method to render the GUI */
 	public void renderGUI() {
 		//Render all of the components
-		for (int a = 0; a < this.components.size(); a++)
-			this.components.get(a).render();
+		for (int a = 0; a < this.panels.size(); a++)
+			this.panels.get(a).renderPanel();
 	}
 	
 	/* The method to show the GUI */
 	public void show() {
 		//Set all of the components to show
-		for (int a = 0; a < this.components.size(); a++)
-			this.components.get(a).visible = true;
+		for (int a = 0; a < this.panels.size(); a++)
+			this.panels.get(a).showPanel();
 	}
 	
 	/* The method to hide the GUI */
 	public void hide() {
 		//Set all of the components to hide
-		for (int a = 0; a < this.components.size(); a++)
-			this.components.get(a).visible = false;
+		for (int a = 0; a < this.panels.size(); a++)
+			this.panels.get(a).hidePanel();
 	}
 	
 	/* The method to add a component */
-	public void add(GUIComponent component) {
+	public void add(GUIPanel panel) {
 		//Add the component to the linked list
-		this.components.add(component);
+		this.panels.add(panel);
 	}
 	
 	/* The method that returns a component given its name */
-	public GUIComponent get(String name) {
+	public GUIPanel get(String name) {
 		//The component
-		GUIComponent component = null;
+		GUIPanel panel = null;
 		//Check the whole list
-		for (int a = 0; a < this.components.size(); a++) {
+		for (int a = 0; a < this.panels.size(); a++) {
 			//Check if it has the right name
-			if (this.components.get(a).name.equals(name))
+			if (this.panels.get(a).name.equals(name))
 				//Assign the component
-				component = this.components.get(a);
+				panel = this.panels.get(a);
 		}
 		//Check if the component is null
-		if (component == null)
+		if (panel == null)
 			//Log a message
-			Logger.log("GUI get()" , "The component with the name " + name + " was not found");
+			Logger.log("GUI get()" , "The panel with the name " + name + " was not found");
 		//Return the component
-		return component;
+		return panel;
 	}
 	
 }
