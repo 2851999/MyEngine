6c7d25304d5a47dac546e1cc20f8b8e26a5e1e3e
2851999 <joelvdavies@btinternet.com>
2013-08-20 13:49:11 +0100

    * Development 11 | Experimental 1 *
    * Changed build number system
    * Added AI class
    * Planning to attempt to add an AI interface and Sprites to combine
    entities and animations

---
+++ MyEngine-Master/src/org/simplecorporation/myengine/core/ai/AI.java
@@ -0,0 +1,15 @@
+/***********************************************
+ * SIMPLE CORPORATION
+ * 
+ * MYENGINE
+ * 
+ * COPYRIGHT @ 2013
+ * 
+ * USE - EDUCATIONAL PURPOSES ONLY
+ ***********************************************/
+
+package org.simplecorporation.myengine.core.ai;
+
+public class AI {
+	
+}
