38b40ca4c291e90436d3685f8c188646bcecd721
2851999 <joelvdavies@btinternet.com>
2013-08-22 08:44:54 +0100

    * Development 11 | Experimental 2 *
    * Renamed AI to BaseAI
    * Added the method updateAI() in BaseAI
    * Added AI

--- MyEngine-Master/src/org/simplecorporation/myengine/core/ai/AI.java
+++ MyEngine-Master/src/org/simplecorporation/myengine/core/ai/AI.java
@@ -10,6 +10,19 @@
 
 package org.simplecorporation.myengine.core.ai;
 
-public class AI {
+public abstract class AI implements BaseAI {
+	
+	/* The name of the AI */
+	public String name;
+	
+	/* The description of the AI*/
+	public String description;
+	
+	/* The constructor */
+	public AI(String name , String description) {
+		//Assign the variables
+		this.name = name;
+		this.description = description;
+	}
 	
 }
