23f125461e2855e53167b60177706093bed6c3a8
2851999 <joelvdavies@btinternet.com>
2013-10-31 13:29:28 +0000

    * Development 14 | Experimental 15 *
    * GUITextBox now makes text slide accross instead of limiting the text
    input

--- MyEngine-Master/src/org/simplecorporation/myengine/core/gui/textbox/GUIImageTextBox.java
+++ MyEngine-Master/src/org/simplecorporation/myengine/core/gui/textbox/GUIImageTextBox.java
@@ -50,18 +50,12 @@ public class GUIImageTextBox extends GUITextBox {
 	/* The method to render the text box */
 	protected void renderComponent() {
 		if (this.visible) {
-			
-			//Check to see if the text fits in the box
-			if (this.font.getWidth(this.text) > this.width - this.font.fontSize / 2)
-				//Remove the last letter
-				this.text = this.text.substring(0 , this.text.length() - 1);
-			
 			//Render the box
 			BasicRenderer.setColour(Colour.WHITE);
 			BasicRenderer.renderImage(this.image , this.position.x , this.position.y , this.width , this.height);
 			
 			//The text to be rendered
-			String renderedText = this.text;
+			String renderText = this.text;
 			
 			//The colour of the text
 			Colour renderColour = Colour.WHITE;
@@ -74,25 +68,39 @@ public class GUIImageTextBox extends GUITextBox {
 			//Check if the characters are hidden
 			if (this.hideCharacters) {
 				//Set the rendered text to nothing
-				renderedText = "";
+				renderText = "";
 				//Loop the text's length
 				for (int a = 0; a < this.text.length(); a++)
 					//Add the masked character
-					renderedText += this.maskCharacter;
+					renderText += this.maskCharacter;
 			}
 			
 			//Check if the default text should be rendered
 			if (this.text.equals("") && ! this.defaultText.equals("") && ! this.selected) {
 				//Set the rendered text
-				renderedText = this.defaultText;
+				renderText = this.defaultText;
 				//Set the colour
 				//renderColour.a = renderColour.a / 2; THIS LINE STOPPED RENDERING (I DONT KNOW HOW TO FIX)
+			}
+			
+			//Check to see if the text fits in the box
+			if (this.font.getWidth(renderText) > this.width - this.font.fontSize / 2) {
+				//Set the render text
+				int beginIndex = 1;
+				//While the text is too long
+				while (this.font.getWidth(renderText.substring(beginIndex)) > this.width - this.font.fontSize / 2) {
+					//Increase the begin index if possible
+					if (renderText.length() > beginIndex + 1)
+						beginIndex ++;
+				}
+				//Set the text
+				renderText = renderText.substring(beginIndex);
 			}
 			
 			//Check if the cursor is showing
 			if (this.cursorShown) {
 				//Add the cursor
-				renderedText += "|";
+				renderText += "|";
 			}
 			
 			//Check if the java font  is null
@@ -102,11 +110,11 @@ public class GUIImageTextBox extends GUITextBox {
 			
 			//Render the text
 			if (Settings.Video.OpenGL)
-				this.font.render(renderedText , this.position.x + 4 ,
+				this.font.render(renderText , this.position.x + 4 ,
 						(this.position.y + (this.height / 2)) - (this.font.getHeight(this.text) / 2));
 			else
 				//Not a clue why it can't be the same as OpenGL
-				this.font.render(renderedText , this.position.x + 4 ,
+				this.font.render(renderText , this.position.x + 4 ,
 						(this.position.y + (this.height / 2)) + (this.font.getHeight(this.text) / 4));
 		}
 	}
