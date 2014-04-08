85b4c6e040e5bf01b6c1b67638602b5a6f5028fc
2851999 <joelvdavies@btinternet.com>
2014-04-08 11:26:10 +0100

    * Development 16 | Beta 0 | Development 1 | Experimental 2 *
    * Added the method load(String filePath) in AndroidAudio
    * Added the method load(String filePath, boolean external) in
    AndroidAudio
    
    * Changed getFile() in AndroidFileUtils, 'external' was geting the
    directory the wrong way round
    
    * Removed 'OpenGL' restrictions with AndroidAudio
    
    * NOTE: Added Android audio loading of files from internal or external
    storage

--- MyEngine-Master/src/org/simplecorporation/myengine/core/audio/clip/AndroidAudio.java
+++ MyEngine-Master/src/org/simplecorporation/myengine/core/audio/clip/AndroidAudio.java
@@ -10,10 +10,17 @@
 
 package org.simplecorporation.myengine.core.audio.clip;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+
 import org.simplecorporation.myengine.core.Settings;
 import org.simplecorporation.myengine.core.android.AndroidActivityListener;
 import org.simplecorporation.myengine.core.android.AndroidSettings;
 import org.simplecorporation.myengine.core.android.AndroidStore;
+import org.simplecorporation.myengine.core.logger.Log;
+import org.simplecorporation.myengine.core.logger.LogType;
+import org.simplecorporation.myengine.core.logger.Logger;
+import org.simplecorporation.myengine.utils.AndroidFileUtils;
 
 import android.media.MediaPlayer;
 
@@ -39,7 +46,7 @@ public class AndroidAudio extends AndroidActivityListener {
 		this.wasPlayingBeforePause = false;
 	}
 	
-	/* The constructor with the file path given */
+	/* The constructor with the file id given */
 	public AndroidAudio(int id , boolean loop , boolean music) {
 		//Assign the variables
 		this.loop = loop;
@@ -49,39 +56,88 @@ public class AndroidAudio extends AndroidActivityListener {
 		this.load(id);
 	}
 	
+	/* The constructor with the filePath given */
+	public AndroidAudio(String filePath , boolean loop , boolean music) {
+		//Assign the variables
+		this.loop = loop;
+		this.music = music;
+		this.wasPlayingBeforePause = false;
+		//Load the audio
+		this.load(filePath);
+	}
+	
+	/* The constructor with the filePath and external boolean value given */
+	public AndroidAudio(String filePath, boolean external, boolean loop , boolean music) {
+		//Assign the variables
+		this.loop = loop;
+		this.music = music;
+		this.wasPlayingBeforePause = false;
+		//Load the audio
+		this.load(filePath, external);
+	}
+	
 	/* The method to start then audio playing */
 	public void play() {
 		//Check which sound to start
-		if (! Settings.Video.OpenGL) {
-			if (this.music)
-				this.androidAudioClip.setVolume((float)((double)Settings.Audio.MusicVolume / (double)100) , (float)((double)Settings.Audio.MusicVolume / (double)100));
-			else if (! this.music)
-				this.androidAudioClip.setVolume((float)((double)Settings.Audio.SoundEffectVolume / (double)100) , (float)((double)Settings.Audio.SoundEffectVolume / (double)100));
-			this.androidAudioClip.setLooping(loop);
-			this.androidAudioClip.start();
-		}
+		if (this.music)
+			//Set the music volume
+			this.androidAudioClip.setVolume((float)((double)Settings.Audio.MusicVolume / (double)100) , (float)((double)Settings.Audio.MusicVolume / (double)100));
+		else if (! this.music)
+			//Set the sound effect volume
+			this.androidAudioClip.setVolume((float)((double)Settings.Audio.SoundEffectVolume / (double)100) , (float)((double)Settings.Audio.SoundEffectVolume / (double)100));
+		//Make sure the clip loops if needed
+		this.androidAudioClip.setLooping(loop);
+		//Start the audio clip
+		this.androidAudioClip.start();
 	}
 	
 	/* The method it stop the audio playing */
 	public void stop() {
-		if (! Settings.Video.OpenGL)
-			this.androidAudioClip.stop();
+		//Stop the audio clip
+		this.androidAudioClip.stop();
 	}
 	
 	/* The method to load the audio */
 	public void load(int id) {
-		//Check whether OpenGL is being used
-		if (! Settings.Video.OpenGL)
-			this.androidAudioClip = MediaPlayer.create(AndroidStore.gameActivity , id);
+		//Create the audio clip
+		this.androidAudioClip = MediaPlayer.create(AndroidStore.gameActivity , id);
+	}
+	
+	/* The method used to load the audio using the file path */
+	public void load(String filePath) {
+		//Create the media player
+		this.androidAudioClip = new MediaPlayer();
+		//Try and catch statement
+		try {
+			//Set the data source of the audio clip
+			this.androidAudioClip.setDataSource(new FileInputStream(filePath).getFD());
+			//Prepare the audio
+			this.androidAudioClip.prepare();
+		} catch (IllegalArgumentException e) {
+			//Log an error
+			Logger.log(new Log("AndroidAudio load(String filePath)", "IllegalArgumentException when loading the file " + filePath, LogType.ERROR));
+		} catch (SecurityException e) {
+			//Log an error
+			Logger.log(new Log("AndroidAudio load(String filePath)", "SecurityException when loading the file " + filePath, LogType.ERROR));
+		} catch (IllegalStateException e) {
+			//Log an error
+			Logger.log(new Log("AndroidAudio load(String filePath)", "IllegalStateException when loading the file " + filePath, LogType.ERROR));
+		} catch (IOException e) {
+			//Log an error
+			Logger.log(new Log("AndroidAudio load(String filePath)", "IOException when loading the file " + filePath, LogType.ERROR));
+		}
+	}
+	
+	/* The method used to load the audio using the file path and whether the file
+	 * is external */
+	public void load(String filePath, boolean external) {
+		//Load the audio
+		this.load(AndroidFileUtils.getFile(filePath, external).toString());
 	}
 	
 	/* The method that returns whether the audio is playing */
 	public boolean isPlaying() {
-		//Check whether OpenGL is being used
-		if (! Settings.Video.OpenGL)
-			return this.androidAudioClip.isPlaying();
-		else
-			return false;
+		return this.androidAudioClip.isPlaying();
 	}
 	
 	/* The method called when the activity pauses */
